2823a7e33e1ce06a8e767259eda33545f91547c8
Diana33n <146648888+Diana33n@users.noreply.github.com>
2024-12-21 16:52:22 +0200

    Update package.json

--- package.js
+++ package.js
@@ -1,37 +1,17 @@
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
-// Require Express to run server and routes
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const port = 3000;
-
-// Start up an instance of app
-const app = express();
-
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-app.use(cors());
-
-// Initialize the main project folder
-app.use(express.static('website'));
-
-// GET endpoint to get the project data
-app.get('/projectData', function (req, res) {
-    res.json(projectData);
-});
-
-app.post('/projectData', function (req, res) {
-    projectData = req.body;
-    res.json({
-        "success": true,
-    });
-});
-
-// Setup Server
-app.listen(port, () => console.log(`listening on port ${port}!`));
+{
+  "name": "weather-journal-app",
+  "version": "1.0.0",
+  "description": "## Overview This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI.",
+  "main": "server.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "node server.js"
+  },
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "body-parser": "^1.19.0",
+    "cors": "^2.8.5",
+    "express": "^4.17.1"
+  }
+}
